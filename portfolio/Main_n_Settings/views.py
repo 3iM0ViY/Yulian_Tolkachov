4c63b2758fdc40bb226285e29938fc450efe5848
Yulian Tolkachov <56gymnasia@gmail.com>
2024-10-08 22:42:49 +0300

    Limit to 5 post on main
    Also testing and bd case solved

--- portfolio/Main_n_Settings/views.py
+++ portfolio/Main_n_Settings/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import *
 from Work.models import *
 # Create your views here.
 
 def index(request):
-	works = Work.objects.filter(is_published=True)
+	works_list = Work.objects.filter(is_published=True)
+	paginator = Paginator(works_list, 5)
+	page_number = request.GET.get('page', 1)
+	works = paginator.page(page_number)
+	
 	context = {
 		"works": works,
 	}
